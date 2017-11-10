ff316862bb0387550630113d976ea75d897a4511
alex-cov <alexcvzz@yahoo.it>
2017-11-10 20:42:37 +0100

    changed to MVP presenter

--- SystemNotification/app/src/main/java/com/alexcovizzi/systemnotification/cpu/CpuFragment.java
+++ SystemNotification/app/src/main/java/com/alexcovizzi/systemnotification/cpu/CpuFragment.java
@@ -5,19 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.alexcovizzi.systemnotification.R;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class CpuFragment extends Fragment implements CpuContract.View {
+public class CpuFragment extends Fragment implements CpuContract.UserActionsListener {
     
-    @BindView(R.id.text_view_cpu_usage) TextView mTextViewCpuUsage;
-    @BindView(R.id.text_view_cpu_max_freq) TextView mTextViewCpuMaxFreq;
-    @BindView(R.id.text_view_cpu_cores) TextView mTextViewCpuCores;
-    @BindView(R.id.text_view_cpu_temp) TextView mTextViewCpuTemp;
+    private CpuContract.View cpuView;
     
     public CpuFragment() {
     
@@ -31,31 +22,12 @@ public class CpuFragment extends Fragment implements CpuContract.View {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_cpu, container, false);
-        
-        ButterKnife.bind(this, rootView);
-        
-        return rootView;
+        cpuView = new CpuView(inflater, container, this);
+        return cpuView.getRoot();
     }
     
     @Override
-    public void showCpuUsage(String usage) {
-        mTextViewCpuUsage.setText(usage);
-    }
+    public void showStatusBarNotification() {
     
-    @Override
-    public void showCpuMaxFreq(String maxFreq) {
-        mTextViewCpuMaxFreq.setText(maxFreq);
-    }
-    
-    @Override
-    public void showCpuCores(String cores) {
-        mTextViewCpuCores.setText(cores);
-    }
-    
-    @Override
-    public void showCpuTemp(String temp) {
-        mTextViewCpuTemp.setText(temp);
     }
 }
